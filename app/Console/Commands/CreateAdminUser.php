6e7cb10383b514fdd80a593a44441dcbcfc33499
faissaloux <fwahabali@gmail.com>
2023-07-22 21:43:17 +0100

    UserSeeder, CreateAdminUser and .env

--- app/Console/Commands/CreateAdminUser.php
+++ app/Console/Commands/CreateAdminUser.php
@@ -25,8 +25,8 @@ final class CreateAdminUser extends Command
 
     protected function createUser(): void
     {
-        $email = $this->ask('Email Address', 'admin@laravel.cm');
-        $name = $this->ask('Name', 'Laravel Cameroun');
+        $email = $this->ask('Email Address', 'admin@laravelmaroc.com');
+        $name = $this->ask('Name', 'Admin');
         $username = $this->ask('Username', 'admin');
         $password = $this->secret('Password');
         $confirmPassword = $this->secret('Confirm Password');
