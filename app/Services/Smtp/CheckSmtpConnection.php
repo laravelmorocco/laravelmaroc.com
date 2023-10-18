1bbc5a1639ce4ce5b19b28c9af09e105637a2afa
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2023-10-18 11:20:40 +0100

    add Class to check Smtp Connection v-2

--- app/Services/Smtp/CheckSmtpConnection.php
+++ app/Services/Smtp/CheckSmtpConnection.php
@@ -2,17 +2,12 @@
 
 namespace App\Services\Smtp;
 
-use Illuminate\Support\Facades\Http;
 use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
 
 class CheckSmtpConnection
 {
     public static function isConnected(): bool
     {
-
-        /*if (!self::check(config('mail.mailers.smtp.host'))) {
-            return false;
-        }*/
 
         try {
             $transport = new EsmtpTransport(config('mail.mailers.smtp.host'), config('mail.mailers.smtp.port'), config('mail.mailers.smtp.encryption'));
@@ -23,18 +18,6 @@ class CheckSmtpConnection
             return true;
         } catch (\Exception $e) {
             //return info($e->getMessage(), ['error_smtp']);
-            return false;
-        }
-    }
-
-    private static function check($domainUrl): bool
-    {
-        try {
-            $response = Http::timeout(3)->get($domainUrl);
-
-            return $response->successful();
-        } catch (\Illuminate\Http\Client\ConnectionException $e) {
-
             return false;
         }
     }
